6cd1e651c547b7ef2ca0de16209cd3e3c8e31d6c
valtsu23 <valtteri.eklund@gmail.com>
2025-08-21 11:37:06 +0300

    Update Can_Bus_example.py

--- Can_Bus_example.py
+++ Can_Bus_example.py
@@ -4,6 +4,7 @@
 
 import can
 import os
+import time
 
 # Set the bitrate 500 kbs in this case
 os.system('sudo ip link set can0 type can bitrate 500000')
@@ -12,20 +13,19 @@ os.system('sudo ifconfig can0 up')
 
 can_bus = can.Bus(channel="can0", interface="socketcan")
 
-while True:
-    # Read can bus message
-    message = can_bus.recv(timeout=1)
-    if message is None:
-        print("No message received")
-    else:
-        print("Message: ", message.data)
-        print("Message ID: ", message.arbitration_id)
-        print("Message data length: ", message.dlc)
+# Read can bus message
+message = can_bus.recv(timeout=5)
+if message is None:
+    print("No message received")
+else:
+    print("Message: ", message.data)
+    print("Message ID: ", message.arbitration_id)
+    print("Message data length: ", message.dlc)
 
-    # Construct message and send
-    msg = can_bus.Message(arbitration_id=0x400, data=[0, 1, 2, 3, 4, 5, 6, 7])
-    can_bus.send(msg)
-    print("Message sent")
+# Construct message and send
+msg = can.Message(arbitration_id=0x400, data=[0, 1, 2, 3, 4, 5, 6, 7])
+can_bus.send(msg)
+print("Message sent")
 
 # Shut down
 can_bus.shutdown()
