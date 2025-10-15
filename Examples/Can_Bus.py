0cc73f5256ab4d35688468412404151995bc0ed5
valtsu23 <valtteri.eklund@gmail.com>
2025-10-15 20:48:34 +0300

    Update Can_Bus.py

--- Examples/Can_Bus.py
+++ Examples/Can_Bus.py
@@ -12,6 +12,11 @@ os.system('sudo ifconfig can0 up')
 
 can_bus = can.Bus(channel="can0", interface="socketcan")
 
+# Construct message and send
+msg = can.Message(arbitration_id=0x500, data=[0, 1, 2, 3, 4, 5, 6, 7])
+can_bus.send(msg)
+print("Message sent")
+
 # Read can bus message
 message = can_bus.recv(timeout=5)
 if message is None:
@@ -20,12 +25,7 @@ else:
     print("Message: ", message.data)
     print("Message ID: ", message.arbitration_id)
     print("Message data length: ", message.dlc)
-
-# Construct message and send
-msg = can.Message(arbitration_id=0x400, data=[0, 1, 2, 3, 4, 5, 6, 7])
-can_bus.send(msg)
-print("Message sent")
-
+    
 # Shut down
 can_bus.shutdown()
 os.system('sudo ifconfig can0 down')
